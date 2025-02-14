94d4f31c26ad0789463d6d6a5e1dba095a77906f
William Wedemire <william.wedemire@ontariotechu.net>
2025-02-13 19:51:25 -0500

    Changed main.py to contain a function to compute the first 20 elements of the fibonacci series

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 print("Hello world!")
+
+newList = []
+
+for i in range(20):
+    newList.append(i)
+
+def fibonacci_series(i):
+    if i == 0:
+        return 0
+    if i <= 2:
+        return 1
+    
+    return fibonacci_series(i - 1) + fibonacci_series(i - 2)
+
+fib_list = []
+
+for i in range(20):
+    fib_list.append(fibonacci_series(i))
+
+print(newList)
+print(fib_list)
